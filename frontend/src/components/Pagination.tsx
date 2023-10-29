111fb9fe2158ce5aa0a4602fbec955f61ad1f989
Galen Casstevens <galencasstevens@gmail.com>
2023-10-29 03:24:21 -0400

    fixed issue with logic to resolve typescript errors

--- frontend/src/components/Pagination.tsx
+++ frontend/src/components/Pagination.tsx
@@ -11,7 +11,7 @@ const Pagination = () => {
 	const { activePage, TOTAL_ITEMS_PER_PAGE } = useAppSelector(
 		(state) => state.pagination
 	);
-	const TOTAL_PAGES = Math.ceil(players?.length ?? 0 / TOTAL_ITEMS_PER_PAGE);
+	const TOTAL_PAGES = Math.ceil((players?.length ?? 0) / TOTAL_ITEMS_PER_PAGE);
 	const isCurrentPageFirst = activePage === 1;
 	const isCurrentPageLast = activePage === TOTAL_PAGES;
 	let isPageNumberOutOfRange: boolean;
