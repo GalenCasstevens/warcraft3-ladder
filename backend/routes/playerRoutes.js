656377bdd8c0896a4c99fb8736416211d9e52a87
Galen Casstevens <galencasstevens@gmail.com>
2024-06-25 21:41:25 -0400

    removed unused backend folder

--- backend/routes/playerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const db = require('../config/db');
-const { getPlayersByExp } = require('../controllers/playerController');
-
-router.route('/').get(getPlayersByExp);
-
-module.exports = router;
